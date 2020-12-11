59f4e1ae3cc63c0f0e1d78f54039efe6ad05dd38
kumars9 <santhoshk.kumar@corteva.com>
2020-12-11 17:22:19 -0600

    added powershell script

---
+++ NewServiceAppRoleAssignment.ps1
@@ -0,0 +1,14 @@
+﻿# Replace with your managed identity object ID
+$miObjectID = "29634640-3e5e-497a-bf92-99595792e2dc"
+# Replace with the permissions required by your app
+$permissionToAdd = "MIFuncAppReadAccessRole"
+
+#FunctionAPP EnterPrise Application Client Id
+$appId = "c04bdd78-1323-4f2a-a218-07223049e72f"
+
+Connect-AzureAD
+
+$app = Get-AzureADServicePrincipal -Filter "AppId eq '$appId'"
+$role = $app.AppRoles | where Value -Like $permissionToAdd | Select-Object -First 1
+New-AzureADServiceAppRoleAssignment -Id $role.Id -ObjectId $miObjectID -PrincipalId $miObjectID -ResourceId $app.ObjectId
+ 
